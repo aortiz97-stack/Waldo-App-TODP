a581e767f82a55a644f094894a16c45aa6d2991c
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-22 14:28:36 -0500

    Add image to game body and adjust header format

--- wheres-waldo-app/src/components/MainGame.js
+++ wheres-waldo-app/src/components/MainGame.js
@@ -1,18 +1,25 @@
-import waldo from '../images/waldo.png';
+import waldo from '../images/waldo-profile.png';
 import odlaw from '../images/odlaw.png';
 import wenda from '../images/wenda.png';
 import wizard from '../images/wizard-white-beard.png';
 import woof from '../images/woof.png';
+import wheresWaldo from '../images/wheres-waldo.jpeg';
+import allCharacters from '../images/all-characters.png';
+
 
 const MainGame = () => {
     return (
         <div id="main-game" style={{display: 'none'}}>
             <div id="header">
+                <div id="header-profile-images">
+                    <img src={allCharacters}/>
+                </div>
                 <div id="timer"><h1>00:00:00</h1></div>
-                <div id="message-board"></div>
-                <div id="counter-left"><h1>Characters Left: 5</h1></div>
+                <div id="counter-left"><h2>Characters Left: 5</h2></div>
             </div>
-            <div id="game-body"></div>
+            <div id="game-body">
+                <img src={wheresWaldo} id="waldo-game-img"/>
+            </div>
         </div>
     );
 };
