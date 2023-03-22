b2390a6f0181ae8a40662a4de89a94e0bfbfa94e
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-22 11:25:19 -0500

    Correct some formats of house page

--- wheres-waldo-app/src/components/HomePage.js
+++ wheres-waldo-app/src/components/HomePage.js
@@ -3,36 +3,50 @@ import odlaw from '../images/odlaw.png';
 import wenda from '../images/wenda.png';
 import wizard from '../images/wizard-white-beard.png';
 import woof from '../images/woof.png';
+import {useEffect} from 'react';
 
 const HomePage = () => {
+    useEffect(() => {
+        const startButton = document.querySelector('button#start');
+        const homePage = document.querySelector('#home-page');
+
+        startButton.addEventListener('click', () => {
+
+        });
+    },
+    [])
     return (
       <div id="home-page">
-        <h1>Where's Waldo?</h1>
-        <p>See if you can find all of the characters in time!</p>
+        <div id="home-text">
+            <h1>Where's Waldo?</h1>
+            <p>See if you can find all of the characters in time!</p>
+        </div>
         <div id="profile-images">
-            <div className="profile-image">
-                <img src={waldo} alt="Waldo"/>
+            <div className="profile">
+                <div className="profile-image-container"><img src={waldo} alt="Waldo"/></div>
                 <p>Waldo</p>
             </div>
-            <div className="profile-image">
-                <img src={odlaw} alt="Odlaw"/>
+            <div className="profile">
+                <div className="profile-image-container"><img src={odlaw} alt="Odlaw"/></div>
                 <p>Odlaw</p>
             </div>
-            <div className="profile-image">
-                <img src={wenda} alt="Wenda"/>
+            <div className="profile">
+                <div className="profile-image-container"><img src={wenda} alt="Wenda"/></div>
                 <p>Wenda</p>
             </div>
-            <div className="profile-image">
-                <img src={wizard} alt="Wizard White Beard" />
+            <div className="profile">
+                <div className="profile-image-container"><img src={wizard} alt="Wizard White Beard" /></div>
                 <p>Wizard White Beard</p>
             </div>
-            <div className="profile-image" id="woof-profile-image-container">
-                <img src={woof} alt="Woof the dog" id="woof" />
+            <div className="profile">
+                <div className="profile-image-container"><img src={woof} alt="Woof the dog" /></div>
                 <p>Woof</p>
             </div>
         </div>
-        <button id="start">Start</button>
-        <button id="scoreboard">ScoreBoard</button>
+        <div id="home-button-container">
+            <button id="start">Start</button>
+            <button id="scoreboard">ScoreBoard</button>
+        </div>
       </div>
     );
   }
