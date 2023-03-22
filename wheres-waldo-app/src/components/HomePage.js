639512834a9dc76b1255f04f028a3fd0b0af1ea6
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-22 13:44:42 -0500

    Begin formatting basic structure of game's header

--- wheres-waldo-app/src/components/HomePage.js
+++ wheres-waldo-app/src/components/HomePage.js
@@ -26,23 +26,23 @@ const HomePage = () => {
         <div id="profile-images">
             <div className="profile">
                 <div className="profile-image-container"><img src={waldo} alt="Waldo"/></div>
-                <p>Waldo</p>
+       
             </div>
             <div className="profile">
                 <div className="profile-image-container"><img src={odlaw} alt="Odlaw"/></div>
-                <p>Odlaw</p>
+   
             </div>
             <div className="profile">
                 <div className="profile-image-container"><img src={wenda} alt="Wenda"/></div>
-                <p>Wenda</p>
+       
             </div>
             <div className="profile">
                 <div className="profile-image-container"><img src={wizard} alt="Wizard White Beard" /></div>
-                <p>Wizard White Beard</p>
+              
             </div>
             <div className="profile">
                 <div className="profile-image-container"><img src={woof} alt="Woof the dog" /></div>
-                <p>Woof</p>
+             
             </div>
         </div>
         <div id="home-button-container">
--- wheres-waldo-app/src/components/MainGame.js
+++ wheres-waldo-app/src/components/MainGame.js
@@ -1,7 +1,17 @@
+import waldo from '../images/waldo.png';
+import odlaw from '../images/odlaw.png';
+import wenda from '../images/wenda.png';
+import wizard from '../images/wizard-white-beard.png';
+import woof from '../images/woof.png';
+
 const MainGame = () => {
     return (
         <div id="main-game" style={{display: 'none'}}>
-            <div id="header"></div>
+            <div id="header">
+                <div id="timer"><h1>00:00:00</h1></div>
+                <div id="message-board"></div>
+                <div id="counter-left"><h1>Characters Left: 5</h1></div>
+            </div>
             <div id="game-body"></div>
         </div>
     );
